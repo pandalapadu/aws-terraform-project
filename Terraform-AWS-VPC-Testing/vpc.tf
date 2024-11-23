ea6e7f3daa58b16f57f962efb776a675d9ee5fd8
Venkat Kunam <venkat@gmail.com>
2024-11-23 14:12:28 +0530

    vpc added

--- Terraform-AWS-VPC-Testing/vpc.tf
+++ Terraform-AWS-VPC-Testing/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                = "../Terraform-AWS-VPC"
+  source                = "git::https://github.com/pandalapadu/aws-terraform-project.git"
   project_name          = var.project_name
   environment           = var.environment
   common_tags           = var.common_tags
